c6fd7f9c7bc47114e1babdfc2ea06b3ac50fabcb
Tad Thorley <phaedryx@gmail.com>
2019-03-31 18:45:21 -0600

    more variables, less magic numbers

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
 import React, { useState }  from 'react';
 import Cell from './components/Cell';
-import GameStatus from './components/GameStatus';
 
 const xChar = "&#x1D5EB;";
 const oChar = "&#x1D5E2;";
-const emptyChar = "&#9642;";
+const eChar = "&#9642;";
+const X = 1;
+const O = -1;
+const E = 0;
 
 export default function App() {
-  const [board, setBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  const [currentPlayer, setCurrentPlayer] = useState(1);
+  const [board, setBoard] = useState([E, E, E, E, E, E, E, E, E]);
+  const [currentPlayer, setCurrentPlayer] = useState(X);
 
   function reset() {
-    setBoard([0, 0, 0, 0, 0, 0, 0, 0, 0]) 
-    setCurrentPlayer(1);
+    setBoard([E, E, E, E, E, E, E, E, E]) 
+    setCurrentPlayer(X);
   }
   
   function togglePlayer() {
-    currentPlayer === 1 ? setCurrentPlayer(-1) : setCurrentPlayer(1)
+    currentPlayer === X ? setCurrentPlayer(O) : setCurrentPlayer(X)
   }
 
   function currentChar() {
-    return currentPlayer === 1 ? xChar : oChar;
+    return currentPlayer === X ? xChar : oChar;
   }
 
   function charAt(pos) {
-    if (board[pos] === 1) return xChar;
-    if (board[pos] === -1) return oChar;
+    if (board[pos] === X) return xChar;
+    if (board[pos] === O) return oChar;
 
-    return emptyChar;
+    return eChar;
   }
 
   function playAt(pos) {
-    if(board[pos] === 0) {
+    if(board[pos] === E) {
       board[pos] = currentPlayer;
       setBoard(board);
       togglePlayer();
@@ -43,46 +45,61 @@ export default function App() {
   // 6 7 8
   function xWin() {
     // across
-    if(board[0] + board[1] + board[2] === 3) return true;
-    if(board[3] + board[4] + board[5] === 3) return true;
-    if(board[6] + board[7] + board[8] === 3) return true;
+    if(board[0] + board[1] + board[2] === 3 * X) return true;
+    if(board[3] + board[4] + board[5] === 3 * X) return true;
+    if(board[6] + board[7] + board[8] === 3 * X) return true;
     // down
-    if(board[0] + board[3] + board[6] === 3) return true;
-    if(board[1] + board[4] + board[7] === 3) return true;
-    if(board[2] + board[5] + board[8] === 3) return true;
+    if(board[0] + board[3] + board[6] === 3 * X) return true;
+    if(board[1] + board[4] + board[7] === 3 * X) return true;
+    if(board[2] + board[5] + board[8] === 3 * X) return true;
     //diagonal
-    if(board[0] + board[4] + board[8] === 3) return true;
-    if(board[6] + board[4] + board[2] === 3) return true;
+    if(board[0] + board[4] + board[8] === 3 * X) return true;
+    if(board[6] + board[4] + board[2] === 3 * X) return true;
 
     return false;
   }
 
   function oWin() {
     // across
-    if (board[0] + board[1] + board[2] === -3) return true;
-    if (board[3] + board[4] + board[5] === -3) return true;
-    if (board[6] + board[7] + board[8] === -3) return true;
+    if (board[0] + board[1] + board[2] === 3 * O) return true;
+    if (board[3] + board[4] + board[5] === 3 * O) return true;
+    if (board[6] + board[7] + board[8] === 3 * O) return true;
     // down
-    if (board[0] + board[3] + board[6] === -3) return true;
-    if (board[1] + board[4] + board[7] === -3) return true;
-    if (board[2] + board[5] + board[8] === -3) return true;
+    if (board[0] + board[3] + board[6] === 3 * O) return true;
+    if (board[1] + board[4] + board[7] === 3 * O) return true;
+    if (board[2] + board[5] + board[8] === 3 * O) return true;
     //diagonal
-    if (board[0] + board[4] + board[8] === -3) return true;
-    if (board[6] + board[4] + board[2] === -3) return true;
+    if (board[0] + board[4] + board[8] === 3 * O) return true;
+    if (board[6] + board[4] + board[2] === 3 * O) return true;
 
     return false;
   }
 
-  function tie() { return board.every(val => val !== 0) }
+  function tie() { return board.every(val => val !== E) }
+
+  function status() {
+    let message = '';
+    if (xWin()) {
+      message = `${xChar} wins the game!`
+    } else if (oWin()) {
+      message = `${oChar} wins the game!`
+    } else if (tie()) {
+      message = 'Tie game!'
+    } else {
+      message = `It is ${currentChar()}'s turn`
+    }
+    return message;
+  }
 
   const cells = board.map((_value, index) =>
     <Cell key={index} pos={index} char={ charAt(index) } playAt={ playAt } />
   )
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
       <div id="gamegrid">{ cells }</div>
-      <GameStatus xWin={xWin} oWin={oWin} tie={tie} currentChar={currentChar} />
+      <div id="status" dangerouslySetInnerHTML={{ __html: status() }} />
       <button onClick={reset}>Reset</button>
     </>
   )
